23c5ebc3320d25e2ec694966fc1a62f495473b88
mzaid04 <150179006+mzaid04@users.noreply.github.com>
2023-12-12 23:57:14 +0500

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Assignment.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("dbConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+       
+        public DbSet<Enrollment> Enrollment { get; set; }
+
+        public DbSet<Class> Class { get; set; }
+
+        public DbSet<Faculty> Faculty { get; set; }
+
+        public DbSet<Student> Student { get; set; }
+
+        
+    }
+}
